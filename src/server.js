d11878318d76d68fe6185b843b01d4f9e4981e06
Serhii-Onykiienko <onikienkosergey@gmail.com>
2026-09-25 00:00:22 +0300

    completed module-1

--- src/server.js
+++ src/server.js
@@ -1,8 +1,57 @@
 // src/server.js
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid, hostname',
+        messageFormat:
+          '{req.method} {req.url} {req.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.post('/users', (req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: 'User created' });
+});
+
+app.get('/error', (req, res, next) => {
+  const error = new Error('Тестова помилка');
+
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 // // Перший маршрут
 // app.get('/', (req, res) => {
@@ -33,36 +82,36 @@ const PORT = 3000;
 // });
 
 // Логування часу
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
 
-// Маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' });
-});
+// // Маршрут
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello, World!' });
+// });
 
-// Маршрут для тестування middleware помилки
-app.get('/test-error', (req, res) => {
-  // Штучна помилка для прикладу
-  throw new Error('Something went wrong');
-});
+// // Маршрут для тестування middleware помилки
+// app.get('/test-error', (req, res) => {
+//   // Штучна помилка для прикладу
+//   throw new Error('Something went wrong');
+// });
 
-// Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+// // Middleware 404 (після всіх маршрутів)
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Route not found' });
+// });
 
-// Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
-  });
-});
+// // Middleware для обробки помилок
+// app.use((err, req, res, next) => {
+//   console.error('Error:', err.message);
+//   res.status(500).json({
+//     message: 'Internal Server Error',
+//     error: err.message,
+//   });
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
